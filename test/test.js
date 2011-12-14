117d0f3a53e46d0e3887417a32acb4cfc557be13
Trevor Norris <trev.norris@gmail.com>
2011-12-14 00:41:29 -0500

    all triggers can now be passed context, but to pass an Array as context without arguments then have to do trigger([ ctx ], []).

--- test/test.js
+++ test/test.js
@@ -14,23 +14,13 @@ var obj0 = new Defer( true ),
 
 obj0promise
 	.done(function( a ) { log( 'obj0 : ' + a + ' : done' ); })
-	.done(function() { log( 'done again' ); })
+	.done(function() { log( this ); })
 	.fail(function( a ) { log( 'obj0 : ' + a + ' : fail' ); })
 	.always(function( a ) { log( 'obj0 : ' + a + ' : always ' ); });
 
-var obj1 = new Defer(),
-	obj1promise = obj1.promise();
-
-obj1promise
-	.done(function( a ) { log( 'obj1 : ' + a + ' : done' ); })
-	.done(function() { log( 'done again' ); })
-	.fail(function( a ) { log( 'obj1 : ' + a + ' : fail' ); });
-
-
-obj1.resolve([ 'resolve' ]).sometimes([ 'sometimes' ]).reject([ 'reject' ]);
-log( '---------' );
 obj0.resolve([ 'resolve' ]).sometimes([ 'sometimes' ]).reject([ 'reject' ]);
 log( '---------' );
+obj0.resolve({ hi : 'me' }).resolve([ 'resolve' ]).resolve({ hi : 'me' },[ 'resolve' ]);
 log( obj0 );
 log( '---------' );
 log( obj0promise );
